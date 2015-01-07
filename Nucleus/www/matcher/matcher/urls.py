01c52e1f869d69153a9adf44f0546330554f0080
brahim_bequan <brahim.bequan@gmail.com>
2015-01-07 16:25:33 +0100

    python url handling + dummy pagina's
    Django kan nu url's handlen naar de juiste pagina's

--- Nucleus/www/matcher/matcher/urls.py
+++ Nucleus/www/matcher/matcher/urls.py
@@ -5,7 +5,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^home', 'website.views.home', name='home'),
+    url(r'^building', 'website.views.building', name='building'),
+    url(r'^producten', 'website.views.producten', name='producten'),
+    url(r'^about-us', 'website.views.aboutus', name='aboutus'),
+    url(r'^contact', 'website.views.contact', name='contact'),
+    
 
+    #admin-page #[example] url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 )
--- Nucleus/www/matcher/website/views.py
+++ Nucleus/www/matcher/website/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def home(request):
 	return render_to_response("main/home.html", {'hello': "Hello World!"});
+
+def building(request):
+	return render_to_response("main/building.html", {'hello': "Hello World!"});
+
+def producten(request):
+	return render_to_response("main/producten.html", {'hello': "Hello World!"});
+
+def aboutus(request):
+	return render_to_response("main/aboutus.html", {'hello': "Hello World!"});
+
+def contact(request):
+	return render_to_response("main/contact.html", {'hello': "Hello World!"});
