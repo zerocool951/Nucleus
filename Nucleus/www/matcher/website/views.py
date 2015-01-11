ce9a8726717c5b03678d90bb703bec4aed87bb1a
brahim_bequan <brahim.bequan@gmail.com>
2015-01-11 14:55:48 +0100

    [dummy-phase] Opzetje van alle pagina's
    Building is in 3 views, omdat dit overzichtelijker is in het
    programmateur.

--- Nucleus/www/matcher/website/views.py
+++ Nucleus/www/matcher/website/views.py
@@ -6,8 +6,17 @@ from django.http import HttpResponse
 def home(request):
 	return render_to_response("main/home.html", {'hello': "Hello World!"});
 
-def building(request):
-	return render_to_response("main/building.html", {'hello': "Hello World!"});
+def building1(request):
+	return render_to_response("main/building-1.html", {'hello': "Hello World!"});
+
+def building2(request):
+	return render_to_response("main/building-2.html", {'hello': "Hello World!"});
+
+def building3(request):
+	return render_to_response("main/building-3.html", {'hello': "Hello World!"});
+
+def finish(request):
+	return render_to_response("main/finish.html", {'hello': "Hello World!"});
 
 def producten(request):
 	return render_to_response("main/producten.html", {'hello': "Hello World!"});
